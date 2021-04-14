d08b3b8d0d87ffb96f25df74116f51d843918cf3
Maxim <aryewar@gmail.com>
2021-04-14 18:42:18 +0300

    Add IsSelected and Arrow Move

--- UMLDesigner/Figures/Arrows/AbstractArrow.cs
+++ UMLDesigner/Figures/Arrows/AbstractArrow.cs
@@ -13,6 +13,7 @@ namespace UMLDesigner.Figures.Arrows
 
         private bool _selectedEnd;
         private bool _selectedStart;
+        private int _deltaXY = 10;
         protected Painter _painter;
 
         public AbstractArrow()
@@ -30,12 +31,42 @@ namespace UMLDesigner.Figures.Arrows
         }
         public bool IsSelected(Point currentPoint)
         {
-            throw new NotImplementedException();
+            if (currentPoint.X >= FinishPoint.X - _deltaXY
+              && currentPoint.X <= FinishPoint.X + _deltaXY
+              && currentPoint.Y >= FinishPoint.Y - _deltaXY
+              && currentPoint.Y <= FinishPoint.Y + _deltaXY)
+            {
+                _selectedStart = false;
+                _selectedEnd = true;
+                return _selectedEnd;
+            }
+            else if (currentPoint.X >= StartPoint.X - _deltaXY
+               && currentPoint.X <= StartPoint.X + _deltaXY
+               && currentPoint.Y >= StartPoint.Y - _deltaXY
+               && currentPoint.Y <= StartPoint.Y + _deltaXY)
+            {
+                _selectedStart = true;
+                _selectedEnd = false;
+                return _selectedStart;
+            }
+            else
+            {
+                _selectedStart = false;
+                _selectedEnd = false;
+                return _selectedStart;
+            }
         }
 
         public void Move(Point currentPoint)
         {
-            throw new NotImplementedException();
+            if (_selectedStart)
+            {
+                StartPoint = currentPoint;
+            }
+            else if (_selectedEnd)
+            {
+               FinishPoint = currentPoint;
+            }
         }
     }
 }
